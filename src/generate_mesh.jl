a5b827ed33d121f54eb8507ffe10fa4fdad20b26
Paul Mendez <pmendez@ing-mat.udec.cl>
2018-06-21 18:21:35 -0400

    Optimizations on mesh generator

--- src/generate_mesh.jl
+++ src/generate_mesh.jl
@@ -18,16 +18,15 @@ function _generate_2d_nodes!(nodes, nx, ny, LL, LR, UR, UL)
 end
 
 function _build_face_data(nodes, el_nodes, el_faces, normals, orientation)
-    coords = [nodes[j].x for j in el_nodes]
-    a = coords[2]-coords[1]
-    b = coords[3]-coords[1]
+    a = nodes[el_nodes[2]].x-nodes[el_nodes[1]].x
+    b = nodes[el_nodes[3]].x-nodes[el_nodes[1]].x
     if (a[1]*b[2]-a[2]*b[1]) < 0
         #swap vertices 2 and 3
         el_nodes[2],el_nodes[3] = el_nodes[3],el_nodes[2]
         el_faces = reverse(el_faces)
     end
     for (i,k) in enumerate(((2,3),(3,1),(1,2)))
-        v1 =  coords[k[2]] - coords[k[1]]
+        v1 =  nodes[el_nodes[k[2]]].x - nodes[el_nodes[k[1]]].x
         n1 = Vec{2}([v1[2], -v1[1]])
         normals[i] = n1/norm(n1)
         #Compute faces orientation
@@ -43,7 +42,7 @@ function _build_cells(cells, el_nodes, n_el, faces, nodes)
         c_face = [el_nodes[fn_id[1]],el_nodes[fn_id[2]]]
         face_found = false
         for (j,face) in enumerate(faces)
-            if sort(face.nodes) == sort(c_face)
+            if (face.nodes[1] == c_face[1] && face.nodes[2] == c_face[2]) || (face.nodes[1] == c_face[2] && face.nodes[2] == c_face[1])
                 el_faces[i] = j
                 if !(n_el in face.cells)
                     push!(face.cells,n_el)
@@ -63,8 +62,8 @@ function _build_cells(cells, el_nodes, n_el, faces, nodes)
     _build_face_data(nodes, el_nodes, el_faces, normals, orientation)
 
     #save cell
-    N = length(el_nodes); M = length(el_faces)
-    cell = Cell(NTuple{N}(el_nodes), NTuple{M}(el_faces), orientation, normals)
+    M = length(el_faces)
+    cell = Cell(el_nodes, NTuple{M}(el_faces), orientation, normals)
     push!(cells, cell)
 end
 
@@ -99,16 +98,14 @@ function rectangle_mesh(::Type{RefTetrahedron}, ::Type{Val{2}}, nel::NTuple{2,In
 
     # Generate cells
     node_array = reshape(collect(1:n_nodes), (n_nodes_x, n_nodes_y))
-    cells = Cell[]
+    cells = TriangleCell[]
     n_el = 0
     for j in 1:nel_y, i in 1:nel_x
-        el_nodes = [node_array[i,j], node_array[i+1,j], node_array[i,j+1]]
         n_el = n_el + 1
-        _build_cells(cells, el_nodes, n_el, faces, nodes) # ◺
+        _build_cells(cells, (node_array[i,j], node_array[i+1,j], node_array[i,j+1]), n_el, faces, nodes) # ◺
 
-        el_nodes = [node_array[i+1,j], node_array[i+1,j+1], node_array[i,j+1]]
         n_el = n_el + 1
-        _build_cells(cells, el_nodes, n_el, faces, nodes) # ◹
+        _build_cells(cells, (node_array[i+1,j], node_array[i+1,j+1], node_array[i,j+1]), n_el, faces, nodes) # ◹
     end
 
     # Add faces sets
